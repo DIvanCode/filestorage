6bcb7b0e9293cd0e2442a338a0fabdd91e71e626
divancode <divancode@gmail.com>
2026-04-28 19:02:57 +0300

    feat: env variables in config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,12 +1,12 @@
 package config
 
 type Config struct {
-	RootDir string        `yaml:"root_dir"`
-	Trasher TrasherConfig `yaml:"trasher"`
+	RootDir string        `yaml:"root_dir" env:"ROOT_DIR"`
+	Trasher TrasherConfig `yaml:"trasher" env-prefix:"TRASHER_"`
 }
 
 type TrasherConfig struct {
-	Workers                  int `yaml:"workers"`
-	CollectorIterationsDelay int `yaml:"collector_iterations_delay"`
-	WorkerIterationsDelay    int `yaml:"worker_iterations_delay"`
+	Workers                  int `yaml:"workers" env:"WORKERS"`
+	CollectorIterationsDelay int `yaml:"collector_iterations_delay" env:"COLLECTOR_ITERATIONS_DELAY"`
+	WorkerIterationsDelay    int `yaml:"worker_iterations_delay" env:"WORKER_ITERATIONS_DELAY"`
 }
